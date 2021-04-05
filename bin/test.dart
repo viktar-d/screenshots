7b3dc00e4e5f7537792269cb75a60ae5f0089e68
Alejandro Ávila <aavilaco@gmail.com>
2021-04-05 20:23:12 +0200

    update

--- bin/test.dart
+++ bin/test.dart
@@ -5,7 +5,6 @@ import 'package:screenshots3/screenshots.dart';
 import 'package:screenshots3/src/config.dart';
 import 'package:screenshots3/src/daemon_client.dart';
 import 'package:screenshots3/src/orientation.dart';
-import 'package:screenshots3/src/run.dart';
 
 void main() async {
   final dev = Device(
@@ -26,5 +25,4 @@ void main() async {
 
   final config = Config(tests: [], locales: [], devices: [dev], sdkPath: 'C:\\src\\android-sdk');
 
-  await setDeviceLocale(config, dev, 'en-US', client);
 }
